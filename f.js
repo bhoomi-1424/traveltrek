a247dd9dc5edc07ff57baabc7a57627c1a2e3c73
bhoomika <bhoomika03082@gmail.com~>
2024-09-01 17:32:36 +0530

    second commit

--- f.js
+++ f.js
@@ -1,5 +1,5 @@
 var num1=parseInt(window.prompt("enter the first number"));
-var num2=parseInt(window.prompt("enter the second number"));
+var num2=parseInt(window.prompt("enterrrrrrrrrrrrr the second number"));
 
 var result=num1+num2;
 document.write("sum of two number "+result);
