14bc309f784e069d97f13e1dd3449b65cd1d207a
bhoomika <bhoomika03082@gmail.com~>
2024-09-01 16:37:32 +0530

    first commit

---
+++ f.js
@@ -0,0 +1,5 @@
+var num1=parseInt(window.prompt("enter the first number"));
+var num2=parseInt(window.prompt("enter the second number"));
+
+var result=num1+num2;
+document.write("sum of two number "+result);
